42fc3ecf179c0637c444606ea12b6c3f46997729
GacinhoV33 <fgacek@student.agh.edu.pl>
2023-02-20 16:46:02 +0100

    Code refactor, add_colors and replace_line methods added to astract class

--- src/colors_scrapper.py
+++ src/colors_scrapper.py
@@ -39,6 +39,14 @@ class FileScrapper(ABC):
         self.colors_groups = colors_groups
         self.path = path
     
+    def update_colors_variables(self, color: str):
+        if color not in self.colors_groups:
+            self.colors_groups[color] = f'variable_{color}'
+    
+    def replace_color_in_line(self, line: str, color: str) -> str:
+        return line.replace(color, self.colors_groups[color])
+
+
     @abstractmethod
     def scrap_file() -> None:
         pass
@@ -72,45 +80,22 @@ class HTMLScrapper(FileScrapper):
 
                         self.update_colors_variables(color)
                         line = self.replace_color_in_line(line, color)
+                        print(line)
                         
             print(line, end="")
-                
-    def update_colors_variables(self, color: str):
-        if color not in self.colors_groups:
-            self.colors_groups[color] = f'variable_{color}'
-    
-    def replace_color_in_line(self, line: str, color: str) -> str:
-
-        return line.replace(color, self.colors_groups[color])
-
                 
 class CSSScrapper(FileScrapper):
     def __init__(self, colors_groups: dict, path: str) -> None:
         super().__init__(colors_groups, path)
 
     def scrap_file(self,) -> None:
-        with open(self.path, 'r') as file:
-            lines = file.readlines()
-            line_number = 0
-            while line_number < len(lines):
-                words = lines[line_number].split(' ')
-                for css_attribute in CSS_COLOR_ATTRIBUTES:
-                    for i, word in enumerate(words):
-                        if css_attribute in word and i < len(words):
-                            lines[line_number] = self.find_var_return_line(words, i)           
-                            print(lines[line_number])
-                line_number +=1
-        os.remove(self.path)
-        with open(self.path, 'w') as file:
-            file.writelines(lines)
+        for line in fileinput.input(self.path, inplace=True):
+            words = line.split(' ')
+            for css_attribute in CSS_COLOR_ATTRIBUTES:
+                for i, word in enumerate(words):
+                    if css_attribute == word and i < len(words):
+                        color = words[i + 1].split(';')[0]
+                        self.update_colors_variables(color)
+                        line = self.replace_color_in_line(line, color) 
+            print(line, end="")
 
-        print(self.colors_groups)
-
-    def find_var_return_line(self, words: list, index: int):
-        #TODO convert to abstract method format 
-        current_color = words[index + 1].split(';')[0]
-        if not self.colors_groups.get(current_color):
-            self.colors_groups[current_color] = f'variable_{current_color}'
-
-        words[index+1] = self.colors_groups[current_color]
-        return ' '.join(words) + ';\n'
